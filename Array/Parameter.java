47c5b2efa681f8ed7c62cd071bc65549dd8a4188
sky103 <a17733971486@163.com>
2025-02-04 11:36:50 +0800

    命令行参数

---
+++ Array/Parameter.java
@@ -0,0 +1,19 @@
+// package Array;
+
+// public class Parameter {
+//     public static void main(String []args){
+//         // java 程序入口是main方法，main方法可以接受一个命令行参数，它是一个Spring[]数组
+//         // 这个命令行参数由JVM接收用户输入并传给main方法
+//         // 我们可以利用接收到的命令行参数,根据不同的参数执行不同的代码,比如实现-version参数,打印打印版本号
+//         // for(String arg:args){
+//         //     if("-version".equals(arg)){
+//         //         System.out.println("v 1.0");
+//         //         break;
+//         //     }
+//         //     // System.out.println(arg);
+//         // }
+//         // 命令行参数类型是String[]数组；
+//         // 命令行参数由JVM接收用户输入并传给main方法；
+
+//     }
+// }
